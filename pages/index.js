1547bbe7bd16555edd5ab6735ab736fbf30c5827
Manaragabriel <gabrielmanara2010@hotmail.com>
2021-05-30 17:54:09 -0300

    continuing cards

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,12 @@ export default function Home() {
         <section class="p-5">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col-lg-4 mt-3">
                         <div class="card border-none text-center ">
                            <div class="d-flex justify-content-center">
                               <img src="/familias.png" class="img-cards"/>
                             </div>
-                            <div class="px-4">
+                            <div class="px-4 py-4">
                               <h3 class=" py-2 blue-dark titles-cards">
                                 Familias Carentes
                             </h3>
@@ -27,7 +27,112 @@ export default function Home() {
                                 Ajude familias e crianças a superarem a pobreza e fome para terem esperanças em um futuro melhor.  
                             </span>
                             <div class="text-center">
-                              <a class=" donation-button donation-button btn col-2 my-2">
+                              <a class=" donation-button donation-button btn col-3 my-2">
+                                  Doe
+                              </a>
+                            </div>
+                           </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 mt-3">
+                        <div class="card border-none text-center ">
+                           <div class="d-flex justify-content-center">
+                              <img src="/saude.png" class="img-cards"/>
+                            </div>
+                            <div class="px-4 py-4">
+                              <h3 class=" py-2 blue-dark titles-cards">
+                                Saúde
+                            </h3>
+                            <span class="description-cards blue-light ">
+                              Ajude hospitais a vencer a guerra do Covid, simples gestos grandes mudanças.  
+                            </span>
+                            <div class="text-center">
+                              <a class=" donation-button donation-button btn col-3 my-2">
+                                  Doe
+                              </a>
+                            </div>
+                           </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 mt-3">
+                        <div class="card border-none text-center ">
+                           <div class="d-flex justify-content-center">
+                              <img src="/educacao.png" class="img-cards"/>
+                            </div>
+                            <div class="px-4 py-4">
+                              <h3 class=" py-2 blue-dark titles-cards">
+                                Educação
+                            </h3>
+                            <span class="description-cards blue-light ">
+                                Contribua com projetos voltados a educação para melhorar cada vez mais a sociedade!  
+                            </span>
+                            <div class="text-center">
+                              <a class=" donation-button donation-button btn col-3 my-2">
+                                  Doe
+                              </a>
+                            </div>
+                           </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 mt-3">
+                        <div class="card border-none text-center ">
+                           <div class="d-flex justify-content-center">
+                              <img src="/animais.png" class="img-cards"/>
+                            </div>
+                            <div class="px-4 py-4">
+                              <h3 class=" py-2 blue-dark titles-cards">
+                                Animais Carentes
+                            </h3>
+                            <span class="description-cards blue-light ">
+                                Ajude os animaizinhos de Mogi Mirim.  
+                            </span>
+                            <div class="text-center">
+                              <a class=" donation-button donation-button btn col-3 my-2">
+                                  Doe
+                              </a>
+                            </div>
+                           </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 mt-3">
+                        <div class="card border-none text-center ">
+                           <div class="d-flex justify-content-center">
+                              <img src="/meio-ambiente.png" class="img-cards"/>
+                            </div>
+                            <div class="px-4 py-4">
+                              <h3 class=" py-2 blue-dark titles-cards">
+                                Meio Ambiente
+                            </h3>
+                            <span class="description-cards blue-light ">
+                                Ajude ambientes a serem revitalizados.  
+                            </span>
+                            <div class="text-center">
+                              <a class=" donation-button donation-button btn col-3 my-2">
+                                  Doe
+                              </a>
+                            </div>
+                           </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 mt-3">
+                        <div class="card border-none text-center ">
+                           <div class="d-flex justify-content-center">
+                              <img src="/outros.png" class="img-cards"/>
+                            </div>
+                            <div class="px-4 py-4">
+                              <h3 class=" py-2 blue-dark titles-cards">
+                                Outros
+                            </h3>
+                            <span class="description-cards blue-light ">
+                                Conheça nossas causas, e ajude-nos a melhorar o planeta!  
+                            </span>
+                            <div class="text-center">
+                              <a class=" donation-button donation-button btn col-3 my-2">
                                   Doe
                               </a>
                             </div>
